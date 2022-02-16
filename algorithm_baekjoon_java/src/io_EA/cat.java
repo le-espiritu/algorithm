a90dad442f128f3b343f7cf97f67b759c51d90f3
le_espiritu <zkthfnr@gmail.com>
2022-02-16 23:13:31 +0900

    2-16

---
+++ algorithm_baekjoon_java/src/io_EA/cat.java
@@ -0,0 +1,12 @@
+package io_EA;
+
+public class cat {
+	public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+	}
+
+}
