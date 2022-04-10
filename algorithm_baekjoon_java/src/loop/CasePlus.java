f766bf90ec7535948260877ef68d7db3ef3ecd4c
le_espiritu <zkthfnr@gmail.com>
2022-04-10 22:56:52 +0900

    loop algorithm 4-10

---
+++ algorithm_baekjoon_java/src/loop/CasePlus.java
@@ -0,0 +1,33 @@
+package loop;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class CasePlus {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i =1; i<=n; i++) {
+			String numbers = br.readLine();
+			String[] numbersArr=numbers.split(" ");
+			
+			int a = Integer.parseInt(numbersArr[0]);
+			int b = Integer.parseInt(numbersArr[1]);
+			
+			bw.write("Case #"+i+": "+(a+b)+"\n");
+			
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+		
+
+	}
+
+}
