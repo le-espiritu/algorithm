38eda4c86db7968b68680bc277f7ac7bf52044c8
le_espiritu <zkthfnr@gmail.com>
2022-12-18 04:01:16 +0900

    12-17 algorithm 최종풀이 - 미로탐색

--- algorithm_baekjoon_java/src/BFS/MazeExploration.java
+++ algorithm_baekjoon_java/src/BFS/MazeExploration.java
@@ -13,6 +13,8 @@ public class MazeExploration {
 	public static int m;
 	public static int[][] map;
 	public static boolean[][] chk;
+	public static int[] dy = {0,1,0,-1}; // 동남서북순  
+	public static int[] dx = {1,0,-1,0};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,15 +34,35 @@ public class MazeExploration {
 			}
 		}
 		
+		
+		
 		Room room = new Room(1,1);
 		chk[1][1]=true;
+		
 		Queue<Room> q = new LinkedList<>();
 		q.offer(room);
 		while(!q.isEmpty()) {
 			Room curRoom = q.poll();
 			int x = curRoom.x;
 			int y = curRoom.y;
+			
+			int levelCount =0;
+			for(int k =0; k<4; k++) {
+				int nx=x+dx[k];
+				int ny=y+dy[k];
+				if(1<=nx && nx<n+1 && 1<=ny && ny<m+1) {
+					if(map[nx][ny]==1 && chk[nx][ny] ==false) {
+						chk[nx][ny]=true;
+						map[nx][ny] = map[x][y]+1; // 이렇게 하면 같은 레벨의 노드(room)들은 모두 같은 가중치를 갖는다.
+						Room nRoom = new Room(nx,ny);
+						q.offer(nRoom);
+					}
+				}
+				
+			}
 		}
+		
+		System.out.println(map[n][m]);
 			
 
 	}
