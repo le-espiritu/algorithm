f751ffa2aeb0cddcd0cd1750d6b9bccf59f7e29c
Dong-yeon Lee <zkthfnr@naver.com>
2022-12-29 19:50:47 +0900

    12-29 백준1504 특정 정점을 지나는 다익스트라 알고리즘 풀이 완료

--- algorithm_baekjoon_java/src/dijkstra/P1504.java
+++ algorithm_baekjoon_java/src/dijkstra/P1504.java
@@ -4,13 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 // 백준 1504번 문제 풀이 - 다익스트라 알고리즘 
 
+
+// public class 와  그냥 class의 차이 이해하기 
+// class 앞에 public이 붙은경우 자바 파일명과 public이 붙은 class 명이 동일해야 한다.
+class Node implements Comparable<Node>{ 
+	int v;
+	int c;
+	
+	public Node(int v, int c) {
+		this.v = v;
+		this.c = c;
+	}
+	
+	public int compareTo(Node o) {
+		return Integer.compare(this.c, o.c);
+	}
+}
+
 public class P1504 {
+	
+	public static int[] dist;
+	public static List<Node>[] graph;
+	public static final int INF = 200000000;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,13 +41,9 @@ public class P1504 {
 		int n = Integer.parseInt(st.nextToken());
 		int e = Integer.parseInt(st.nextToken());
 		
-		int[] dist;
-		List<Node>[] graph;
 		
 		dist = new int[n+1];
-		for(int i=0; i<dist.length; i++) {
-			dist[i]=Integer.MAX_VALUE;
-		}
+		Arrays.fill(dist, INF);
 		
 		graph=new ArrayList[n+1];
 		for(int i=0; i<graph.length; i++) {
@@ -41,16 +60,39 @@ public class P1504 {
 			graph[to].add(new Node(start,cost));
 		}
 		
+		//반드시 거쳐야하는 정점들 입력받음
 		st= new StringTokenizer(br.readLine());
 		int u = Integer.parseInt(st.nextToken());
 		int v = Integer.parseInt(st.nextToken());
 		
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(1,0));
-		dist[1]=0;
+		int result1 =0;
+		result1 += dijk(1,u);
+		result1 += dijk(u,v);
+		result1 += dijk(v,n);
 		
-		boolean uChck = false;
-		boolean vChck = false;
+		int result2 =0;
+		result2 += dijk(1,v);
+		result2 += dijk(v,u);
+		result2 += dijk(u,n);
+		
+		int answer = Math.min(result1, result2);
+		
+		if(answer >= INF) { // 크거나 같아야함. 왜냐하면 INF 자체도  인피니티 값이니까. 
+			System.out.println(-1);
+		}else {
+			System.out.println(answer);
+		}
+		
+	}
+	
+	
+	public static int dijk(int start, int to) {
+		Arrays.fill(dist, INF);
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start,0));
+		dist[start]=0;
+		
 		
 		while(!pq.isEmpty()) {
 			Node nowNode = pq.poll();
@@ -59,19 +101,9 @@ public class P1504 {
 				continue;
 			}
 			
-			if(nowNode.v==u) {
-				uChck=true;
-			}
-			if(nowNode.v==v) {
-				vChck=true;
-			}
 			
 			for(Node nextNode : graph[nowNode.v]) {
-				
-				if(nextNode.v==n && uChck==false || vChck==false) {
-					continue;
-					
-				}
+		
 				if(dist[nextNode.v]>nowNode.c+nextNode.c) {
 					
 					dist[nextNode.v]=nowNode.c+nextNode.c;
@@ -81,28 +113,10 @@ public class P1504 {
 			}
 		}
 		
-		if(dist[n]==Integer.MAX_VALUE) {
-			System.out.println(-1);
-		}else {
-			System.out.println(dist[n]);
-		}
 		
-
+		return dist[to];
 	}
 	
-	public static class Node implements Comparable<Node>{
-		int v;
-		int c;
-		
-		public Node(int v, int c) {
-			this.v = v;
-			this.c = c;
-		}
 
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.c, o.c);
-		}
-	}
 
 }
