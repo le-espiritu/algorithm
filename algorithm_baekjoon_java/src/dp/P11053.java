18dec392e4c1f45fce7eb1d7bf1f668c63f0e1d3
le_espiritu <zkthfnr@gmail.com>
2023-02-12 03:19:15 +0900

    2-11 algorithm - dp

--- algorithm_baekjoon_java/src/dp/P11053.java
+++ algorithm_baekjoon_java/src/dp/P11053.java
@@ -1,27 +1,45 @@
 package dp;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class P11053 {
+	
+	public static int[] seq;
+	public static Integer[] dp; // null체크를 위해 Integer로 선언. 
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int n = sc.nextInt();
 		
-		ArrayList<Integer> rs = new ArrayList<>();
+		seq=new int[n];
+		dp=new Integer[n];
 		
-		int pre = 0;
 		for(int i=0; i<n; i++) {
-			int num = sc.nextInt();
-			if(num>pre) {
-				rs.add(num);
-				pre=num;
+			seq[i]=sc.nextInt();
+		}
+		
+		int max =0;
+		
+		for(int i=0; i<n; i++) {
+			int rs = LIS(i);
+			max = Math.max(max, rs);
+		}
+		
+		System.out.println(max);
+	
+	}
+	public static int LIS(int idx) {
+		if(dp[idx]==null) {
+			dp[idx]=1;
+			
+			for(int j=idx-1; j>=0; j--) {
+				if(seq[j]<seq[idx]) {
+					dp[idx]=Math.max(dp[idx], LIS(j)+1);
+				}
 			}
 		}
 		
-		System.out.println(rs.size());
-
+		return dp[idx];
 	}
 
 }
