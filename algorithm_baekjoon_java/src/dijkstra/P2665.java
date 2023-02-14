e2d98154ddf8a962fce1b9e57e59b682ceb35078
le_espiritu <zkthfnr@gmail.com>
2023-02-15 01:34:24 +0900

    2-14 algorithm3

--- algorithm_baekjoon_java/src/dijkstra/P2665.java
+++ algorithm_baekjoon_java/src/dijkstra/P2665.java
@@ -3,80 +3,76 @@ package dijkstra;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class P2665 {
+	
+	public static int n;
+	public static int[][]map;
+	public static int[][]dist;
+	
+	public static int[] dy= {0,1,0,-1};
+	public static int[] dx= {1,0,-1,0};
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n =Integer.parseInt(br.readLine());
 		
-		int[][] map = new int[n][n];
-		int[][] dist = new int[n][n];
+		n=Integer.parseInt(br.readLine());
+		
+		map=new int[n][n];
+		dist=new int[n][n];
 		
 		for(int i=0; i<n; i++) {
 			String line = br.readLine();
 			for(int j=0; j<n; j++) {
-				int room = Character.getNumericValue(line.charAt(j));
-				if(room==1) {
-					map[i][j]=0;
-				}else if(room==0) {
-					map[i][j]=1;
-				}
-				
+				map[i][j]= Character.getNumericValue(line.charAt(j));
 				dist[i][j]=Integer.MAX_VALUE;
 			}
 		}
 		
-		int[] dy= {0,1,0,-1};
-		int[] dx= {1,0,-1,0};
+		bfs(0,0);
 		
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		dist[0][0]=0;
-		pq.offer(new Edge(0,0,0));
+		System.out.println(dist[n-1][n-1]);
+
+	}
+	
+	public static void bfs(int y, int x) {
+		Queue<Room> qu = new LinkedList<>();
+		qu.offer(new Room(y,x));
+		dist[y][x]=0;
 		
-		while(!pq.isEmpty()) {
-			Edge nowEdge = pq.poll();
-			
-			int y = nowEdge.y;
-			int x = nowEdge.x;
-			
-			if(nowEdge.cost>dist[y][x]) {
-				continue;
-			}
+		while(!qu.isEmpty()) {
+			Room r = qu.poll();
 			
 			for(int i=0; i<4; i++) {
-				int ny = y+dy[i];
-				int nx = x+dx[i];
+				int ny = r.y+dy[i];
+				int nx = r.x+dx[i];
 				
 				if(ny>=0 && ny<n && nx>=0 && nx<n) {
-					if(dist[ny][nx]>nowEdge.cost+map[ny][nx]) {
-						dist[ny][nx]=nowEdge.cost+map[ny][nx];
-						pq.offer(new Edge(ny,nx,dist[ny][nx]));
+					if(dist[ny][nx]>dist[r.y][r.x]) {
+						if(map[ny][nx]==1) {
+							dist[ny][nx]=dist[r.y][r.x];
+						}
+						else if(map[ny][nx]==0) {
+							dist[ny][nx]=dist[r.y][r.x]+1;
+						}
+						
+						qu.offer(new Room(ny,nx));
 					}
 				}
 			}
 		}
-		
-		System.out.println(dist[n-1][n-1]);
-		
-		
 	}
-	
-	public static class Edge implements Comparable<Edge>{
-		int y;
+
+	public static class Room{
 		int x;
-		int cost;
+		int y;
 		
-		public Edge(int y, int x, int cost) {
+		public Room(int y, int x) {
 			this.y=y;
 			this.x=x;
-			this.cost=cost;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.cost, o.cost);
 		}
 	}
-
+	
 }
---
+++ algorithm_baekjoon_java/src/dijkstra/P2665_2.java
@@ -0,0 +1,82 @@
+package dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class P2665_2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n =Integer.parseInt(br.readLine());
+		
+		int[][] map = new int[n][n];
+		int[][] dist = new int[n][n];
+		
+		for(int i=0; i<n; i++) {
+			String line = br.readLine();
+			for(int j=0; j<n; j++) {
+				int room = Character.getNumericValue(line.charAt(j));
+				if(room==1) {
+					map[i][j]=0;
+				}else if(room==0) {
+					map[i][j]=1;
+				}
+				
+				dist[i][j]=Integer.MAX_VALUE;
+			}
+		}
+		
+		int[] dy= {0,1,0,-1};
+		int[] dx= {1,0,-1,0};
+		
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		dist[0][0]=0;
+		pq.offer(new Edge(0,0,0));
+		
+		while(!pq.isEmpty()) {
+			Edge nowEdge = pq.poll();
+			
+			int y = nowEdge.y;
+			int x = nowEdge.x;
+			
+			if(nowEdge.cost>dist[y][x]) {
+				continue;
+			}
+			
+			for(int i=0; i<4; i++) {
+				int ny = y+dy[i];
+				int nx = x+dx[i];
+				
+				if(ny>=0 && ny<n && nx>=0 && nx<n) {
+					if(dist[ny][nx]>nowEdge.cost+map[ny][nx]) {
+						dist[ny][nx]=nowEdge.cost+map[ny][nx];
+						pq.offer(new Edge(ny,nx,dist[ny][nx]));
+					}
+				}
+			}
+		}
+		
+		System.out.println(dist[n-1][n-1]);
+		
+		
+	}
+	
+	public static class Edge implements Comparable<Edge>{
+		int y;
+		int x;
+		int cost;
+		
+		public Edge(int y, int x, int cost) {
+			this.y=y;
+			this.x=x;
+			this.cost=cost;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.cost, o.cost);
+		}
+	}
+
+}
